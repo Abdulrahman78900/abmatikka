0e0bffb72c7919e0f30737e55f9b9cc4d3ce558a
Abdulrahman <156474819+ab-almafrachi@users.noreply.github.com>
2026-06-20 01:54:17 +0300

    Added hanburger menu button for mobile

--- script.js
+++ script.js
@@ -50,3 +50,16 @@ document.querySelectorAll(".faq-question").forEach(button => {
     faqItem.classList.toggle("active");
   });
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
